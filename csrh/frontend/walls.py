7d1dbb1dec51295cdcf5244bde2815f974cb6eb9
ivfiev <filjko@ya.ru>
2025-11-15 23:36:37 +0000

    set class_

--- csrh/frontend/walls.py
+++ csrh/frontend/walls.py
@@ -13,13 +13,16 @@ mid_y = 2160.0 / 2.0
 mid_x = 3840.0 / 2.0
 px_200_100_350 = 180.0
 
+
 class MovableWindow:
     def __init__(self, root, x, y, h, w):
-        self.root = tk.Toplevel(root)
+        self.root = tk.Toplevel(root, class_="csxax")
         self.root.overrideredirect(True)
         self.root.geometry(f"{w}x{h}+{x}+{y}")
-        self.root.attributes('-topmost', True)
-        self.canvas = tk.Canvas(self.root, width=w, height=h, bg=None, highlightthickness=0)
+        self.root.attributes("-topmost", True)
+        self.canvas = tk.Canvas(
+            self.root, width=w, height=h, bg=None, highlightthickness=0
+        )
         self.canvas.pack()
         self.x, self.y = x, y
         self.h, self.w = h, w
@@ -30,7 +33,9 @@ class MovableWindow:
     def set(self, x, y, h, w, col):
         self.x, self.y, self.h, self.w = x, y, h, w
         self.col = col
-        self.root.geometry(f"{round(self.w)}x{round(self.h)}+{round(self.x - self.w/2)}+{round(self.y - self.h/2)}")
+        self.root.geometry(
+            f"{round(self.w)}x{round(self.h)}+{round(self.x - self.w/2)}+{round(self.y - self.h/2)}"
+        )
         self.canvas.config(width=self.w, height=self.h, bg=col)
 
     def toggle(self, visible):
@@ -42,22 +47,35 @@ class MovableWindow:
         else:
             self.root.withdraw()
 
+
 class Root(tk.Tk):
-    def __init__(self, screenName = None, baseName = None, className = "Tk", useTk = True, sync = False, use = None):
+    def __init__(
+        self,
+        screenName=None,
+        baseName=None,
+        className="Tk",
+        useTk=True,
+        sync=False,
+        use=None,
+    ):
         super().__init__(screenName, baseName, className, useTk, sync, use)
         self.withdraw()
+
 
 root = Root()
 windows = [MovableWindow(root, 0, 0, 0, 0) for _ in range(65)]
 
+
 def fisheye(r):
-    if abs(r) <= math.pi / 7: 
+    if abs(r) <= math.pi / 7:
         return 1
-    return (1 + 0.33 * (abs(r) - math.pi / 7))**2
-    
+    return (1 + 0.33 * (abs(r) - math.pi / 7)) ** 2
+
+
 def targets():
     enemies = [(int(id), e) for id, e in model.players.items() if model.is_enemy(e)]
     return enemies
+
 
 try:
     signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
@@ -73,7 +91,7 @@ try:
                 (id, e) = t
                 active.add(id)
                 win = windows[id]
-                
+
                 dist = util.get_dist(e)
                 if dist == 0:
                     continue
@@ -93,20 +111,20 @@ try:
                 if not visible:
                     h /= fish
                     w /= fish
-                    x = mid_x * 2 - w/5 if x > mid_x else 0 + w/5
+                    x = mid_x * 2 - w / 5 if x > mid_x else 0 + w / 5
                     y = mid_y
 
-                color = 'red' if e.color == 'T' else 'blue'
+                color = "red" if e.color == "T" else "blue"
                 win.set(x, y, h, w, color)
 
                 if not win.visible:
-                    sleep(0.001)
+                    sleep(0.003)
                     win.toggle(True)
 
             for i in range(65):
                 if i not in active:
                     windows[i].toggle(False)
-        
+
         root.update_idletasks()
         root.update()
         sleep(0.001)
